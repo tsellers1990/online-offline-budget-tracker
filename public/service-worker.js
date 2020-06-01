856cc7109697d19d07abf2f4995bc05f72561106
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-01 15:09:55 -0600

    built the manifest and built the install and fetch event listeners!

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "my-site-cache-v1"; //might need to be var... I dont think it does though, reference me if it breaks
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const urlsToCache = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(event) {
+            console.log("Opened Cache");
+            return caches.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if(response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err=> {
+                        return cache.match(event.request);
+                    });
+            })
+        )
+    }
+})
